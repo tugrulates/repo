d5ea0e646e17b8b77ec0c6b6a392c2fcdba22ef5
Tugrul Ates <me@tugrulates.com>
2025-01-21 20:03:15 +0100

    test(500px): subcommand help tests

--- 500px/main_test.ts
+++ 500px/main_test.ts
@@ -12,6 +12,15 @@ Deno.test(
 );
 
 Deno.test(
+  "500px discover --help",
+  async (t) => {
+    using console = fakeConsole();
+    await main(["discover", "--help"]);
+    await assertSnapshot(t, console.calls);
+  },
+);
+
+Deno.test(
   "500px discover",
   async (t) => {
     using console = fakeConsole();
@@ -22,11 +31,29 @@ Deno.test(
 );
 
 Deno.test(
+  "500px follows --help",
+  async (t) => {
+    using console = fakeConsole();
+    await main(["follows", "--help"]);
+    await assertSnapshot(t, console.calls);
+  },
+);
+
+Deno.test(
   "500px follows <username>",
   async (t) => {
     using console = fakeConsole();
     using _fetch = mockFetch(t);
     await main(["follows", "tugrulates"]);
+    await assertSnapshot(t, console.calls);
+  },
+);
+
+Deno.test(
+  "500px photos --help",
+  async (t) => {
+    using console = fakeConsole();
+    await main(["photos", "--help"]);
     await assertSnapshot(t, console.calls);
   },
 );
