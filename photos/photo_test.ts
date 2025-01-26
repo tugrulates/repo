1312280f819f6e24c591c87f9a27a1841be9ab03
Tugrul Ates <me@tugrulates.com>
2025-01-26 16:18:44 +0100

    feat(testing): disposable tempDir (#96)

--- photos/photo_test.ts
+++ photos/photo_test.ts
@@ -3,16 +3,7 @@ import { basename } from "@std/path";
 import { join } from "@std/path/join";
 import { assertSnapshot } from "@std/testing/snapshot";
 import { copyExifToVariants, getPhoto, type Photo } from "@tugrulates/photos";
-
-async function getTestData() {
-  const dir = await Deno.makeTempDir();
-  await copy("photos/testdata", dir, { overwrite: true });
-  return {
-    dir,
-    path: (path: string) => join(dir, path),
-    [Symbol.asyncDispose]: () => Deno.remove(dir, { recursive: true }),
-  };
-}
+import { tempDir } from "../testing/temp.ts";
 
 function trimSource(photo: Photo): Photo {
   return {
@@ -30,8 +21,9 @@ Deno.test(
   // @todo Fix the timer leaks.
   { sanitizeOps: false, sanitizeResources: false },
   async (t) => {
-    await using data = await getTestData();
-    const photo = await getPhoto(data.path("floating-around/source.jpg"));
+    await using dir = await tempDir();
+    await copy("photos/testdata", dir.path, { overwrite: true });
+    const photo = await getPhoto(join(dir.path, "floating-around/source.jpg"));
     await assertSnapshot(t, trimSource(photo));
   },
 );
@@ -40,11 +32,12 @@ Deno.test(
   "copyExifToVariants()",
   { sanitizeOps: false, sanitizeResources: false },
   async (t) => {
-    await using data = await getTestData();
-    const before = await getPhoto(data.path("winter-pause/source.jpg"));
+    await using dir = await tempDir();
+    await copy("photos/testdata", dir.path, { overwrite: true });
+    const before = await getPhoto(join(dir.path, "winter-pause/source.jpg"));
     await assertSnapshot(t, trimSource(before));
     await copyExifToVariants(before);
-    const after = await getPhoto(data.path("winter-pause/source.jpg"));
+    const after = await getPhoto(join(dir.path, "winter-pause/source.jpg"));
     await assertSnapshot(t, trimSource(after));
   },
 );
