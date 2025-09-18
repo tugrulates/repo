95d3b2c89310dc7869ed651cfb3182c20326a810
Tugrul Ates <me@tugrulates.com>
2025-09-18 18:24:49 +0200

    refactor: tidy tests
    - use `assertObjectMatch`
    - use `fakeArgs` and `fakeEnv` from roka
    - use config permissions from Deno 2.5

--- photo/exif.test.ts
+++ photo/exif.test.ts
@@ -1,47 +1,29 @@
-import { assertEquals } from "@std/assert";
-import { type Exif, exif, write } from "./exif.ts";
+import { assertObjectMatch } from "@std/assert";
+import { exif, write } from "./exif.ts";
 import { tempPhoto } from "./testing.ts";
 
-function testExif() {
-  return {
-    title: "test-title",
-    description: "test-description",
-    keywords: ["test-keyword-1", "test-keyword-2"],
-    date: "2000-01-01 00:00:00",
-    location: "test-location",
-    city: "test-city",
-    state: "test-state",
-    country: "test-country",
-    make: "test-make",
-    model: "test-model",
-    lens: "test-lens",
-    software: "test-software",
-    license: "test-license",
-  };
-}
-
-function assertExif(exif: Exif) {
-  assertEquals(exif.title, "test-title");
-  assertEquals(exif.description, "test-description");
-  assertEquals(exif.keywords, ["test-keyword-1", "test-keyword-2"]);
-  assertEquals(exif.date, "2000-01-01T00:00:00-08:00");
-  assertEquals(exif.location, "test-location");
-  assertEquals(exif.city, "test-city");
-  assertEquals(exif.state, "test-state");
-  assertEquals(exif.country, "test-country");
-  assertEquals(exif.make, "test-make");
-  assertEquals(exif.model, "test-model");
-  assertEquals(exif.lens, "test-lens");
-  assertEquals(exif.software, "test-software");
-  assertEquals(exif.license, "test-license");
-}
+const TEST_EXIF = {
+  title: "test-title",
+  description: "test-description",
+  keywords: ["test-keyword-1", "test-keyword-2"],
+  date: "2000-01-01T00:00:00-08:00",
+  location: "test-location",
+  city: "test-city",
+  state: "test-state",
+  country: "test-country",
+  make: "test-make",
+  model: "test-model",
+  lens: "test-lens",
+  software: "test-software",
+  license: "test-license",
+};
 
 Deno.test("exif() returns photo tags", {
   sanitizeOps: false,
   sanitizeResources: false,
 }, async () => {
-  await using photo = await tempPhoto(testExif());
-  assertExif(await exif(photo.path));
+  await using photo = await tempPhoto(TEST_EXIF);
+  assertObjectMatch(await exif(photo.path), TEST_EXIF);
 });
 
 Deno.test("write() can update tags", {
@@ -49,16 +31,16 @@ Deno.test("write() can update tags", {
   sanitizeResources: false,
 }, async () => {
   await using photo = await tempPhoto();
-  await write(photo.path, testExif());
-  assertExif(await exif(photo.path));
+  await write(photo.path, TEST_EXIF);
+  assertObjectMatch(await exif(photo.path), TEST_EXIF);
 });
 
 Deno.test("write() can copy tags from a file", {
   sanitizeOps: false,
   sanitizeResources: false,
 }, async () => {
-  await using photo1 = await tempPhoto(testExif());
+  await using photo1 = await tempPhoto(TEST_EXIF);
   await using photo2 = await tempPhoto();
   await write(photo2.path, { source: photo1.path });
-  assertExif(await exif(photo2.path));
+  assertObjectMatch(await exif(photo2.path), TEST_EXIF);
 });
